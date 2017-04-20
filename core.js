84a7b2469fa22bb3a912cac25a8c0925cd1c1726
Daniel Kerr <danielkerr@Daniels-Mac-mini.local>
2017-04-20 22:48:41 +0100

    Refactoring to .js and .css files

---
+++ core.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+console.log("here");
+Start();
+})
+
+function Start(){
+	$("a").each(function(index){
+	//	
+			if($(this).prop("href"))
+			{
+				console.log(index + ": " + $(this).prop("href"));
+				if($(this).text() && $(this).text().length<2){
+					$(this).text($(this).prop("href"));
+				}
+				
+			}	
+			else
+			{
+				if(console){
+					console.log(index + ": none");
+				}
+			}	
+		});
+
+}
